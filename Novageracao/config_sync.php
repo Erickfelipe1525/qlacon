62ff676ed1c5e783a13e46ce5774dd9a8f2e3c63
Erick Felipe <erickfelipe5424@gmail.com>
2025-11-15 18:54:49 -0300

    Delete Novageracao directory

--- Novageracao/config_sync.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Config para sincronização (mantenha este arquivo fora de alcance público se possível)
-$SYNC_SECRET = 'a7f9c4b6e8d1f2a3b4c5d6e7f8a9b0c1'; // chave gerada — troque se quiser
-?>
